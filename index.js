53df473fe9e5c22dcac38d621b083749339f4ae2
eun-n <eunicena.12@gmail.com>
2016-04-28 16:05:38 -0700

    pokepicture

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ var express = require("express");
 var db = require('./models');
 var authCtrl = require('./controllers/auth');
 var flash = require('express-flash');
-var io = require('socket.io').listen(app);
-
 
 var app = express();
 var session = require('express-session');
-
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(express.static(__dirname + '/static'));
@@ -51,7 +48,6 @@ app.use(function(req, res, next) {
 
 app.get('/', function (req, res) {
   res.render('home');
-  //file.serve(req, res);
 });
 
 app.get('/video/:postid', function (req, res) {
@@ -107,46 +103,5 @@ app.get('/random', function(req, res) {
     }
   });
 });
-
-io.sockets.on('connection', function (socket){
-
-  // convenience function to log server messages on the client
-  function log(){
-    var array = [">>> Message from server: "];
-    for (var i = 0; i < arguments.length; i++) {
-      array.push(arguments[i]);
-    }
-      socket.emit('log', array);
-  }
-
-  socket.on('message', function (message) {
-    log('Got message:', message);
-    // for a real app, would be room only (not broadcast)
-    socket.broadcast.emit('message', message);
-  });
-
-  socket.on('create or join', function (room) {
-    var numClients = io.sockets.clients(room).length;
-
-    log('Room ' + room + ' has ' + numClients + ' client(s)');
-    log('Request to create or join room ' + room);
-
-    if (numClients === 0){
-      socket.join(room);
-      socket.emit('created', room);
-    } else if (numClients === 1) {
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room);
-    } else { // max two clients
-      socket.emit('full', room);
-    }
-    socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-    socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
-
-  });
-
-});
-
 
 app.listen(process.env.PORT || 3000)
